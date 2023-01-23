4ba0c0bce335bfd96a7d93bffdc7ad02624c983f
tzztson <zhenghu61919@gmail.com>
2023-01-24 04:33:45 +0800

    feat: add the main style of project

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,38 @@ import React from "react";
 
 function App() {
   return (
-    <div className="App">
-      <h1 className="bg-red-400 text-white">Hello World</h1>
+    <div className="text-xl">
+      <div className="container mx-auto px-12 max-sm:px-6">
+        <div className="flex flex-col justify-center items-center h-[100vh]">
+          <div className="flex flex-col gap-4 w-full">
+            <p className="text-2xl py-4">Buy Cyrpto Token using Paypal</p>
+            <div className="flex justify-center items-center gap-4 w-full max-xl:flex-col">
+              <p className="flex-[1_1_5%]   w-full">Your Wallet Address:</p>
+              <input
+                type="text"
+                className="px-2 outline-none flex-[1_1_70%] border-black border-b-[1px] w-full"
+                placeholder="Your Wallet Address"
+              />
+            </div>
+            <div className="flex justify-center items-center gap-4 w-full max-xl:flex-col">
+              <p className="flex-[1_1_5%]   w-full">Token Amount:</p>
+              <input
+                type="text"
+                className="px-2 outline-none flex-[1_1_70%]  border-black border-b-[1px] w-full"
+                placeholder="Token Amount"
+              />
+            </div>
+            <div className="flex justify-center items-center gap-4 w-full max-xl:flex-col">
+              <p className="flex-[1_1_5%]   w-full">Paypal Amount:</p>
+              <input
+                type="text"
+                className="px-2 outline-none flex-[1_1_70%]  border-black border-b-[1px] w-full"
+                placeholder="Paypal Amount"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
